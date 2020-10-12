aedbd57512b57ee01765a9fc0215337827c5223f
Christopher Henderson <chenderson5280@gmail.com>
2020-10-12 17:37:06 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |room,name|
-    if room == :suite_a && passengers[room].start_with? ("A")
+    if room == :suite_a && name.start_with? ("A")
        puts name
     end
   end
